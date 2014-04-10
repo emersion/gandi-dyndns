9084b219dac1e52154a3266b098cdefc13cd063b
$imon <contact@emersion.fr>
2014-04-10 15:31:37 +0200

    Create cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apikey='' # API key (e.g. X8AOlxkFLIjPZnDErxRc1T3)
+domain='' # Domain (e.g. example.com)
+record='' # Record (XXXXXX in https://www.gandi.net/admin/domain/zone/XXXXXX/N/edit)
+
+./gandi-dyndns --apikey=$apikey --domain=$domain --record=$record
