42192fb4cfbcc181021e1b3f2c172619f3a59606
$imon <contact@emersion.fr>
2014-04-19 14:41:29 +0200

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -6,4 +6,4 @@ apikey='' # API key (e.g. X8AOlxkFLIjPZnDErxRc1T3)
 domain='' # Domain (e.g. example.com)
 record='' # Record (e.g. @)
 
-python2 "$basedir/gandi-dyndns" --api=$apikey --domain=$domain --record=$record >> "$basedir/cronjob-emersion.log"
+python2 "$basedir/gandi-dyndns" --api=$apikey --domain=$domain --record=$record --cache --cache-file="$basedir/cache.json" >> "$basedir/cronjob.log"
