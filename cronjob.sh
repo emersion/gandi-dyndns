96b7fc86f795ef7c461608d7d1f16d88889546e1
emersion <contact@emersion.fr>
2015-12-26 17:58:14 +0100

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -6,4 +6,4 @@ apikey='' # API key (e.g. X8AOlxkFLIjPZnDErxRc1T3)
 domain='' # Domain (e.g. example.com)
 record='' # Record (e.g. @)
 
-python2 "$basedir/gandi-dyndns" --api=$apikey --domain=$domain --record=$record --cache --cache-file="$basedir/cache.json" >> "$basedir/cronjob.log"
+python2 "$basedir/gandi-dyndns" --api=$apikey --domain=$domain --record=$record --cache --cache-file="$basedir/cache.json" | tee "$basedir/cronjob.log"
